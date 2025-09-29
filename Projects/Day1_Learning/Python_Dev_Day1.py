480e2897fdb8cc5ffd94cb767b19b6780f33592d
MarinoOzkaya <marinoozkaya@gmail.com>
2025-09-30 08:53:47 +0900

    Create Python_Dev_Day1.py

---
+++ Projects/Day1_Learning/Python_Dev_Day1.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator!")
+hometown = input("Where did you grow up?\n")
+petname = input("What is your pet's name?\n")
+bandname = hometown + " " + petname
+print("You should name your band" + " " + bandname + "!")
+
